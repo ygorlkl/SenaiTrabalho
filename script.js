9c838f3eaf4ff192aaede1b1e836386f70662796
ygorlkl <36214552+ygorlkl@users.noreply.github.com>
2022-09-07 13:15:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function função0(){
+    window.location.href="index.html"
+}
+
+function função1(){
+    window.location.href="calunos.html"
+}
+
+function função2(){
+    window.location.href="cturmas.html"
+}
+
+function função3(){
+    window.location.href="cnotas.html"
+}
+
+function função4(){
+    window.location.href="lchamadas.html"
+}
+
+function função5(){
+    window.location.href="consultaralunos.html"
+}
+
+function função6(){
+    window.location.href="consultarturmas.html"
+}
+
